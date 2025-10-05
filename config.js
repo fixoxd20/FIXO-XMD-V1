a402cb1b24b72809250359d462c81a35b5d8ef36
FIXO XD <oshannethsara44@gmail.com>
2025-10-05 09:11:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "FIXO-XMD~UzFxTLTC#imOmUq4zxGC3R6YIviOMc8CeHdXLw3WOxFZqf6u8WGI",
 // add your Session Id
 AUTO_BIO: process.env.AUTO_BIO || "false", 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
